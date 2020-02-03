5ec1027ed2348e006e0a3e8596ed26d286545b7b
chenglon <894436399@qq.com>
2020-02-03 18:28:53 +0800

    first

---
+++ src/main/java/com/cheng/entities/Apple.java
@@ -0,0 +1,14 @@
+package com.cheng.entities;
+
+public class Apple {
+		private  String appName="good apple";
+
+		public String getAppName() {
+			return appName;
+		}
+
+		public void setAppName(String appName) {
+			this.appName = appName;
+		}
+		
+}
